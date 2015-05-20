701d54681c3973caf095b8e320b06d0fcb38df90
Dror Porat <dgingi@gmail.com>
2015-05-20 18:22:17 +0300

    Working crawler - fixed in game parsing

--- crawler.py
+++ crawler.py
@@ -20,8 +20,8 @@ def start_crawl():
     TODO: more generic crawling
     """
     browser = webdriver.Chrome() # Get local session of Chrome
-    browser.get("http://www.whoscored.com/Regions/252/Tournaments/2/Seasons/3853/Stages/7794/Fixtures/England-Premier-League-2013-2014") # Load page
-    browser.implicitly_wait(15)
+    browser.get("http://www.whoscored.com/Regions/252/Tournaments/2/Seasons/3853")
+    browser.implicitly_wait(5)
     parse_league(browser)
     
     browser.close()
@@ -30,95 +30,86 @@ def parse_league(browser):
     """
     Start parsing the league table.
     
-    
-    TODO: parse the other tables
     """
-
+    wait = WebDriverWait(browser,5)
+    
+    table = browser.find_element_by_class_name("stat-table")
+    list_tlinks = table.find_elements_by_class_name("team-link")
+    all_teams_names = set([thr.text for thr in list_tlinks])
+    max_games = 2*len(all_teams_names)-1
+    
+    all_teams_dict = {name:{i:{} for i in range(1,max_games)} for name in all_teams_names}
+    all_teams_curr_fix = {name:1 for name in all_teams_names}
+    
+    fixtures_elm = browser.find_element_by_link_text("Fixtures")
+    fixtures_elm.click()
+    try:
+        wait.until(EC.text_to_be_present_in_element((By.TAG_NAME,"h2"),'Fixture'))
+    finally:
+        pass
+    
     disp_month = browser.find_element_by_id('date-controller')
     prev_month = disp_month.find_elements_by_tag_name('a')[0]
-    months = ['May','Apr','Mar','Feb','Jan','Dec','Nov','Oct','Sep','Aug']
+    months = ['Aug','Sep','Oct','Nov','Dec','Jan','Feb','Mar','Apr','May']
     games_by_month = {i:None for i in months}
-    wait = WebDriverWait(browser,15)
 
-    for i in months:
+    for i in months[::-1]:
         try:
             wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME,"rowgroupheader"),i))
-            games_by_month[i] = browser.find_elements_by_xpath('//a[@class="result-1 rc"]')
+            all_res = browser.find_elements_by_xpath('//a[@class="result-1 rc"]')
+            games_by_month[i] = [(res.get_attribute("href"),res.text) for res in all_res]
             prev_month.click()
         finally: #TODO - catch the TimeOut exception and do something about it
             pass
     
-    for month in games_by_month:
-        for game in month:
-            parse_game(browser,game.get_attribute("href"))    
+    for month in months:
+        for game in games_by_month[month]:
+            parse_game(browser,game[0],all_teams_dict,all_teams_curr_fix)  
     
-    #list_tlinks = browser.find_elements_by_class_name("team-link")
-    #list_thr = set([thr.get_attribute("href") for thr in list_tlinks])
+    with open("PL-13-14.pckl",'w') as output:
+        dump(all_teams_dict, output)  
     
-    #all_teams_names = set([thr.text for thr in list_tlinks])
-    #all_teams_dict = {name:[{},{},{},{}] for name in all_teams_names}
-    
-    #opt_tabels = browser.find_elements_by_id("stage-team-stats-options")[0]
-    #linked_tabels = opt_tabels.find_elements_by_xpath(".//a")[:-1] 
-    
-    #iter=0 
-    
-    #for link_table in linked_tabels:
-    #    link_table.click()
-    #    link_table.parent.implicitly_wait(10)
-    #    rel_table = browser.find_elements_by_id("stage-team-stats-"+link_table.text.lower())[0]
-    #    table = rel_table.find_elements_by_id("top-team-stats-summary-grid")[0]
-    #    league_header = [h.text for h in table.find_element_by_tag_name("thead").find_elements_by_tag_name("th")[2:]]
-    #    league_body = table.find_element_by_tag_name("tbody")
-    #    league_lines = league_body.find_elements_by_tag_name("tr")
-    #    for line in league_lines:
-    #        team_data_line = [l.text for l in line.find_elements_by_tag_name("td")[1:]]
-    #        team_name = team_data_line[0]
-    #        team_data_line = team_data_line[1:]
-    #        all_teams_dict[team_name][iter] = {h:d for h,d in izip(league_header,team_data_line) if h!="Discipline"}
-    #    iter+=1
-           
-    #for link in list_thr:
-    #    parse_team(browser,link,all_teams_dict)
-    
-    #with open("output.pckl",'w') as output:
-    #    dump(all_teams_dict, output)
-
-    
-def parse_game(browser,link,all_teams_dict):
+   
+def parse_game(browser,link,all_teams_dict,all_teams_curr_fix):
     """
     Start parsing a single game (Home VS Away).
-    
-    TODO: parse the other tables
+
     """
     
     browser.get(link)
-    browser.implicitly_wait(10)
+    
     players_stats_elm = browser.find_element_by_link_text("Player Statistics")
     players_stats_elm.click() #TODO - see if need to wait for page to load
     
-    #list_plinks = browser.find_elements_by_class_name("player-link")
-    #list_thr = set([thr.get_attribute("href") for thr in list_plinks])
+    list_tlinks = browser.find_elements_by_class_name("team-link")
+    home_team_name = list_tlinks[0].text
+    away_team_name = list_tlinks[1].text
     
     home_table = browser.find_element_by_id("live-player-home-stats")
     home_players = home_table.find_elements_by_class_name("pn")
     home_players_names = set([player.text.split('\n')[0] for player in home_players])
-    home_players_dict = {name:[{},{},{},{}] for name in home_players_names}
+    home_players_dict = {name:{"summary":{},"offensive":{},"defensive":{},"passing":{}} for name in home_players_names}
+    
     away_table = browser.find_element_by_id("live-player-away-stats")
     away_players = away_table.find_elements_by_class_name("pn")
     away_players_names = set([player.text.split('\n')[0] for player in away_players])
-    away_players_dict = {name:[{},{},{},{}] for name in away_players_names}
+    away_players_dict = {name:{"summary":{},"offensive":{},"defensive":{},"passing":{}} for name in away_players_names}
     
-    team_name = browser.find_element_by_tag_name("h1").text
-    print(team_name)
-    opt_tabels = browser.find_elements_by_id("team-squad-stats-options")[0]
-    linked_tabels = opt_tabels.find_elements_by_xpath(".//a")[:-1] 
+     
+    parse_team(browser,"home",home_players_dict)
+    parse_team(browser,"away",away_players_dict)
     
-    iter=0
+    all_teams_dict[home_team_name][all_teams_curr_fix[home_team_name]]=home_players_dict
+    all_teams_curr_fix[home_team_name]+=1
+    all_teams_dict[away_team_name][all_teams_curr_fix[away_team_name]]=away_players_dict
+    all_teams_curr_fix[away_team_name]+=1
+    
+def parse_team(browser,curr_team,all_players_dict):
+    opt_tabels = browser.find_elements_by_id("live-player-"+curr_team+"-options")[0]
+    linked_tabels = opt_tabels.find_elements_by_xpath(".//a")
     for link_table in linked_tabels:
         link_table.click()
-        link_table.parent.implicitly_wait(10)
-        rel_table = browser.find_elements_by_id("team-squad-stats-"+link_table.text.lower())[0]
+        rel_table = browser.find_elements_by_id("live-player-"+curr_team+"-"+link_table.text.lower())[0]
         table = rel_table.find_elements_by_id("top-player-stats-summary-grid")[0]
         team_header = [h.text for h in table.find_element_by_tag_name("thead").find_elements_by_tag_name("th")[7:]]
         team_body = table.find_element_by_tag_name("tbody")
@@ -126,12 +117,10 @@ def parse_game(browser,link,all_teams_dict):
         for line in team_lines:
             player_data_line = [l.text for l in line.find_elements_by_tag_name("td")[2:]]
             player_name = player_data_line[0].split('\n')[0]
-            player_data_line = player_data_line[5:]
-            all_players_dict[player_name][iter] = {h:d for h,d in izip(team_header,player_data_line) if h!="Discipline"}
-        iter+=1
+            player_data_line = player_data_line[1:]
+            all_players_dict[player_name][link_table.text.lower()] = {h:d for h,d in izip(team_header,player_data_line)}
     
-    all_teams_dict[team_name][3]=all_players_dict
-    
+        
             
 if __name__ == '__main__':
     start_crawl() 
