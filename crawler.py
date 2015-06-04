dc1665e6a93bbb3a4b9830aaa6e05ab1a699dfb7
Ory Jonay <kriegsmeister@gmail.com>
2015-06-04 18:13:34 +0300

    Almost fixed crawler. Saving each month to save time- next time we need to run the crawl from Feb-May. Pickle files included.

--- crawler.py
+++ crawler.py
@@ -12,6 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from itertools import izip
 from pickle import dump,load
+import utils
 
 def start_crawl():
     """
@@ -20,8 +21,8 @@ def start_crawl():
     TODO: more generic crawling
     """
     browser = webdriver.Chrome() # Get local session of Chrome
+    browser.implicitly_wait(30)
     browser.get("http://www.whoscored.com/Regions/252/Tournaments/2/Seasons/3853")
-    browser.implicitly_wait(5)
     parse_league(browser)
     
     browser.close()
@@ -31,14 +32,14 @@ def parse_league(browser):
     Start parsing the league table.
     
     """
-    wait = WebDriverWait(browser,5)
+    wait = WebDriverWait(browser,15)
     
     table = browser.find_element_by_class_name("stat-table")
     list_tlinks = table.find_elements_by_class_name("team-link")
     all_teams_names = set([thr.text for thr in list_tlinks])
-    max_games = 2*len(all_teams_names)-1
     
-    all_teams_dict = {name:{i:{} for i in range(1,max_games)} for name in all_teams_names}
+    
+    all_teams_dict = {name:{i:{} for i in range(1,2*len(all_teams_names)-1)} for name in all_teams_names}
     all_teams_curr_fix = {name:1 for name in all_teams_names}
     
     fixtures_elm = browser.find_element_by_link_text("Fixtures")
@@ -64,36 +65,41 @@ def parse_league(browser):
     
     for month in months:
         for game in games_by_month[month]:
-            parse_game(browser,game[0],all_teams_dict,all_teams_curr_fix)  
+            parse_game(webdriver.Chrome(),game[0],all_teams_dict,all_teams_curr_fix)  
+        else: #saving each month separately
+            with open("PL-13-14-"+month+".pckl",'w') as output:
+                dump(all_teams_dict, output)  
     
-    with open("PL-13-14.pckl",'w') as output:
-        dump(all_teams_dict, output)  
     
-   
+
 def parse_game(browser,link,all_teams_dict,all_teams_curr_fix):
     """
     Start parsing a single game (Home VS Away).
 
     """
-    
+    browser.implicitly_wait(30)
     browser.get(link)
-    
+
     players_stats_elm = browser.find_element_by_link_text("Player Statistics")
     players_stats_elm.click() #TODO - see if need to wait for page to load
+    
+    time.sleep(10)
     
     list_tlinks = browser.find_elements_by_class_name("team-link")
     home_team_name = list_tlinks[0].text
     away_team_name = list_tlinks[1].text
     
+    def create_dicts_from_table(table):
+        players = table.find_elements_by_class_name("pn")
+        players_names = set([player.text.split('\n')[0] for player in players])
+        players_dict = {name:{"summary":{},"offensive":{},"defensive":{},"passing":{}} for name in players_names}
+        return players , players_names , players_dict
+    
     home_table = browser.find_element_by_id("live-player-home-stats")
-    home_players = home_table.find_elements_by_class_name("pn")
-    home_players_names = set([player.text.split('\n')[0] for player in home_players])
-    home_players_dict = {name:{"summary":{},"offensive":{},"defensive":{},"passing":{}} for name in home_players_names}
+    home_players ,home_players_names ,home_players_dict = create_dicts_from_table(home_table) 
     
     away_table = browser.find_element_by_id("live-player-away-stats")
-    away_players = away_table.find_elements_by_class_name("pn")
-    away_players_names = set([player.text.split('\n')[0] for player in away_players])
-    away_players_dict = {name:{"summary":{},"offensive":{},"defensive":{},"passing":{}} for name in away_players_names}
+    away_players ,away_players_names ,away_players_dict = create_dicts_from_table(away_table)
     
      
     parse_team(browser,"home",home_players_dict)
@@ -103,14 +109,15 @@ def parse_game(browser,link,all_teams_dict,all_teams_curr_fix):
     all_teams_curr_fix[home_team_name]+=1
     all_teams_dict[away_team_name][all_teams_curr_fix[away_team_name]]=away_players_dict
     all_teams_curr_fix[away_team_name]+=1
+    browser.close()
     
 def parse_team(browser,curr_team,all_players_dict):
-    opt_tabels = browser.find_elements_by_id("live-player-"+curr_team+"-options")[0]
+    opt_tabels = browser.find_element_by_id("live-player-"+curr_team+"-options")
     linked_tabels = opt_tabels.find_elements_by_xpath(".//a")
     for link_table in linked_tabels:
         link_table.click()
-        rel_table = browser.find_elements_by_id("live-player-"+curr_team+"-"+link_table.text.lower())[0]
-        table = rel_table.find_elements_by_id("top-player-stats-summary-grid")[0]
+        rel_table = browser.find_element_by_id("live-player-"+curr_team+"-"+link_table.text.lower())
+        table = rel_table.find_element_by_id("top-player-stats-summary-grid")
         team_header = [h.text for h in table.find_element_by_tag_name("thead").find_elements_by_tag_name("th")[7:]]
         team_body = table.find_element_by_tag_name("tbody")
         team_lines = team_body.find_elements_by_tag_name("tr")
---
+++ utils.py
@@ -0,0 +1,14 @@
+from time import clock
+
+def timed(f):
+    '''decorator for printing the timing of functions
+    usage: 
+    @timed
+    def some_funcion(args...):'''
+    
+    def wrap(*x, **d):
+        start = clock()
+        res = f(*x, **d)
+        print(f.__name__, ':', clock() - start)
+        return res
+    return wrap
