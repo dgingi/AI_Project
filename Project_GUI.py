5a524534f0b43fd6253e938c2a44d4a5bb0d94d0
dgingi <dgingi@gmail.com>
2015-11-23 12:27:53 +0200

    Just take a look now

--- Project_GUI.py
+++ Project_GUI.py
@@ -2,17 +2,23 @@ from Tkinter import *
 from crawler import *
 from pickle import load
 import subprocess
+import tkMessageBox
+import Tkinter
   
 TITLE_FONT = ("Helvetica", 18, "bold")
 
+def start_crawl_func(args,league,year):
+    if league == "Nothing Selected Yet" or year == "Nothing Selected Yet":
+        tkMessageBox.showinfo("ATTENTION!!!!", "Please select both league and year")
+    else:
+        subprocess.Popen(args+[league,year])
+    
 class SampleApp(Tk):
 
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         Tk.geometry(self, "600x400")
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
+        
         container = Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -23,7 +29,7 @@ class SampleApp(Tk):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-
+        
         self.show_frame(home_page)
 
     def show_frame(self, c):
@@ -35,9 +41,15 @@ class SampleApp(Tk):
 class home_page(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+        
+        background_image = PhotoImage(file = "C:\Users\dror\Documents\Technion\AI-Project\GUI_BG.gif")
+        BGLabel = Label(self, image = background_image)
+        BGLabel.image = background_image
+        BGLabel.place(x=0, y=0, relwidth=1, relheight=1)
+        
         label = Label(self, text="This is the AI Project GUI", font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
-
+        
         crawler_b = Button(self, text="Crawler",
                             command=lambda: controller.show_frame(crawler_page))
         quit_b = Button(self,text="QUIT",bg="red",command=self.quit)
@@ -49,8 +61,23 @@ class crawler_page(Frame):
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+        
+        background_image = PhotoImage(file = "C:\Users\dror\Documents\Technion\AI-Project\GUI_BG.gif")
+        BGLabel = Label(self, image = background_image)
+        BGLabel.image = background_image
+        BGLabel.place(x=0, y=0, relwidth=1, relheight=1)
+        
         self.label = Label(self, text="This is The crawler", font=TITLE_FONT)
         self.label.pack(side="top", fill="x", pady=10)
+        self.label_league = Label(self,height=5,width=38)
+        self.label_league.place(x=13,y=90)
+        self.label_year = Label(self,height=5,width=38)
+        self.label_year.place(x=292,y=90)
+        self.label_league_selection = Label(self,height=1,width=35)
+        self.label_league_selection.place(x=13,y=210)
+        self.label_year_selection = Label(self,height=1,width=33)
+        self.label_year_selection.place(x=292,y=210)
+        
         self.home_b = Button(self, text="Go to the home page",
                            command=lambda: controller.show_frame(home_page))
         self.home_b.pack(side=BOTTOM,pady=30)
@@ -74,6 +101,7 @@ class crawler_page(Frame):
         all_years.reverse()
         for year in all_years:
             self.year_list.insert(END,year)
+        self.year_list.insert(END,"all")
         self.year_list.place(x=407,y=92)
         self.sb2 = Scrollbar(self,orient=VERTICAL)
         self.sb2.place(x=532,y=94)
@@ -81,8 +109,8 @@ class crawler_page(Frame):
         self.year_list.configure(yscrollcommand=self.sb2.set)
         
               
-        self.select_league_b = Button(self, text="SELECT LEAGUE",bg="green",command=lambda: self.return_active(self.league_list,self.league_selection))
-        self.select_league_b.place(x=140,y=170)
+        self.select_league_b = Button(self, text="SELECT LEAGUE",bg="cyan",command=lambda: self.return_active(self.league_list,self.league_selection))
+        self.select_league_b.place(x=140,y=175)
         self.selected_league_l = Label(self,text="Selected League is :")
         self.selected_league_l.place(x=13,y=210)
         self.league_selection = StringVar()
@@ -90,8 +118,8 @@ class crawler_page(Frame):
         self.league_entry = Entry(self,textvariable=self.league_selection)
         self.league_entry.place(x=140,y=210)
         
-        self.select_year_b = Button(self, text="SELECT YEAR",bg="green",command=lambda: self.return_active(self.year_list,self.year_selection))
-        self.select_year_b.place(x=407,y=170)
+        self.select_year_b = Button(self, text="SELECT YEAR",bg="cyan",command=lambda: self.return_active(self.year_list,self.year_selection))
+        self.select_year_b.place(x=407,y=175)
         self.selected_year_l = Label(self,text="Selected Year is :")
         self.selected_year_l.place(x=292,y=210)
         self.year_selection = StringVar()
@@ -107,13 +135,16 @@ class crawler_page(Frame):
                      }
         
         args = ["python","crawler.py"]
-        self.start_crawl = Button(self, text="START CRAWLING",bg="blue",command=lambda : subprocess.Popen(args+[self.league_selection.get(),self.year_selection.get()]))
+        self.start_crawl = Button(self, text="START CRAWLING",bg="green",command=lambda : start_crawl_func(args, self.league_selection.get(), self.year_selection.get()))
         self.start_crawl.pack(side=BOTTOM,pady=40)
         
         
 
     def return_active(self,list,selection):
-        selection.set(list.get(ACTIVE))
+        if list.get(ACTIVE) == "all":
+            selection.set("2010-2014")
+        else:
+            selection.set(list.get(ACTIVE))
     
     
 if __name__ == "__main__":
