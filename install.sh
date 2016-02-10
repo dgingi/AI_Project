20508f4fd53c30604cf1cad066b6f964113cd21c
ory <ory@cronus-cyber.com>
2016-02-10 13:13:41 +0200

    Correction to install script

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-#Installing numpy, scipy, matplotlib, 
-sudo apt-get install build-essential python-dev python-setuptools python-numpy python-scipy libatlas-dev libatlas3gf-base
+#Installing numpy, scipy, matplotlib, pip
+sudo apt-get install -y build-essential python-dev python-setuptools python-numpy python-scipy libatlas-dev libatlas3gf-base
 sudo update-alternatives --set libblas.so.3 /usr/lib/atlas-base/atlas/libblas.so.3
 sudo update-alternatives --set liblapack.so.3 /usr/lib/atlas-base/atlas/liblapack.so.3
-sudo apt-get install python-matplotlib
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-pip
 
 #Installing pyMongo, scikit-learn, Selenium
 sudo -H pip install pymongo
